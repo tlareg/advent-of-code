951a66703f37321720bf6a4b9a66c59d24bbdc96
tlareg <tlaregaivir@gmail.com>
2016-12-01 10:23:36 +0100

    solution 1 wip

--- src/2016/day01/solution.js
+++ src/2016/day01/solution.js
@@ -1,26 +1,43 @@
 "use strict";
 
 const fs = require('fs')
-const inputStr = fs.readFileSync('./input.txt').toString()
+const inputStr = 'R8, R4, R4, R8' //fs.readFileSync('./input.txt').toString()
 
-const answer = parseInput(inputStr)
-console.log(answer)
+const answers = parseInput(inputStr)
+console.log(answers)
 
 function parseInput(inputStr) {
   const inputArr = inputStr.split(',').map(s => s.trim())
 
   let cardinalDirection = 'N'
+  let firstCoordinatesVisitedTwice = null
   const startCoordinates = { x: 0, y: 0 }
+  const history = [startCoordinates]
 
-  const endCoordinates = inputArr.reduce(({x, y}, item) => {
-    const relativeDirection = item[0]
-    const distance = parseInt(item[1], 10)
+  const endCoordinates = inputArr.reduce(({x, y}, itemStr) => {
+    const relativeDirection = itemStr[0]
+    const distance = parseInt(itemStr.slice(1), 10)
+
     cardinalDirection = updateCardinalDirection(cardinalDirection, relativeDirection)
-    return updateCoordinates({x, y}, cardinalDirection, distance)
+    const newCoordinates = updateCoordinates({x, y}, cardinalDirection, distance)
+
+    if (!firstCoordinatesVisitedTwice && isInHistory(history, newCoordinates)) {
+      firstCoordinatesVisitedTwice = newCoordinates
+    }
+
+    console.log(newCoordinates)
+
+    history.push(newCoordinates)
+    return newCoordinates
   }, startCoordinates)
 
   const totalDistance = taxicabDistance(endCoordinates, startCoordinates)
-  return totalDistance
+  const bunnyHqDistance = firstCoordinatesVisitedTwice ? taxicabDistance(firstCoordinatesVisitedTwice, startCoordinates) : null
+
+  return {
+    part1Answer: totalDistance,
+    part2Answer: bunnyHqDistance
+  }
 }
 
 function updateCardinalDirection(cardinalDirection, relativeDirection) {
@@ -44,4 +61,12 @@ function updateCoordinates({x, y}, cardinalDirection, distance) {
 
 function taxicabDistance({ x: xa, y: ya }, { x: xb, y: yb }) {
   return Math.abs(xa - xb) + Math.abs(ya - yb)
+}
+
+function isInHistory(history, coordinates) {
+  return history.some(c => areCoordinatesEqual(c, coordinates))
+}
+
+function areCoordinatesEqual({ x: xa, y: ya }, { x: xb, y: yb }) {
+  return (xa === xb) && (ya === yb)
 }
