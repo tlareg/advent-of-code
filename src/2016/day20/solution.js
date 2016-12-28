13f5f2bc3c24881a529763a3f60d7c7b71d62bb5
tlareg <tlaregaivir@gmail.com>
2016-12-28 18:33:05 +0100

    day20

---
+++ src/2016/day20/solution.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const inputStr = fs.readFileSync('./input.txt', 'utf8').toString()
+const MAX = 4294967295
+
+parseInput(inputStr)
+
+function parseInput(str) {
+  const ranges = str.split('\r\n').map(
+    x => x.split('-').map(i => parseInt(i, 10))
+  ).sort((a, b) => a[0] - b[0])
+
+  let part1;
+  let allowedCount = 0;
+  let maxRangeEnd = ranges[0][1];
+
+  if (ranges[0][0] !== 0) { 
+    part1 = 0
+    allowedCount += ranges[0][0] - 1
+  }
+
+  for (let i = 0; i < ranges.length - 1; i++) {
+    const range = ranges[i]
+    const nextRange = ranges[i + 1]
+    if (maxRangeEnd < nextRange[0] - 1) {
+      if (part1 === undefined) {
+        part1 = maxRangeEnd + 1
+      }
+      allowedCount += (nextRange[0] - 1 - maxRangeEnd)
+    }
+    maxRangeEnd = Math.max(maxRangeEnd, nextRange[1])
+  }
+
+  if (maxRangeEnd < MAX) {
+    if (part1 === undefined) {
+      part1 = maxRangeEnd + 1
+    }
+    allowedCount += MAX - maxRangeEnd
+  }
+
+  console.log(`part1: ${part1}`)
+  console.log(`part2: ${allowedCount}`)
+}
