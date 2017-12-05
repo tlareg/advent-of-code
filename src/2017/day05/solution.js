a80dc8cea85a5db8f603f2a2534279714cf1005e
tlareg <tlaregaivir@gmail.com>
2017-12-05 12:52:36 +0100

    2017 day05

---
+++ src/2017/day05/solution.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const fs = require('fs')
+const inputStr = fs.readFileSync('./input.txt').toString()
+const jumps = inputStr.split('\r\n').map(Number)
+
+const first = solve(jumps, (jumps, pos) => jumps[pos] + 1)
+console.log(first)
+
+const second = solve(jumps, (jumps, pos) =>
+  jumps[pos] >= 3 ? jumps[pos] - 1 : jumps[pos] + 1)
+console.log(second)
+
+function solve(jumps, getNewJump) {
+  jumps = [...jumps]
+  let pos = 0
+  let steps = 0
+  while (pos >= 0 && pos <= jumps.length - 1) {
+    const jump = jumps[pos]
+    jumps[pos] = getNewJump(jumps, pos)
+    pos += jump
+    steps++
+  }
+  return steps
+}
