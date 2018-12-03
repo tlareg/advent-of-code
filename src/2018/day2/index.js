13c70eed1ad5e04cce3fc1a60d8ec29632b947bc
tlareg <tlaregaivir@gmail.com>
2018-12-04 00:46:34 +0100

    2018 day2 second

--- src/2018/day2/index.js
+++ src/2018/day2/index.js
@@ -15,12 +15,29 @@ const firstStar = () => {
 
     return { seen2Times, seen3Times }
   }, {
-    seen2Times: 0,
-    seen3Times: 0
-  })
+      seen2Times: 0,
+      seen3Times: 0
+    })
 
   return seen2Times * seen3Times;
 }
 
 console.log(`first star: ${firstStar(lines)}`)
 
+const diff = (word1, word2) => {
+  const common = [...word1].reduce((acc, char, i) =>
+    (char === word2[i]) ? acc + char : acc, '')
+  return common.length === word1.length - 1 ? common : false;
+}
+
+const secondStar = lines => {
+  let common;
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = i + 1; j < lines.length; j++) {
+      common = diff(lines[i], lines[j])
+      if (common) return common;
+    }
+  }
+}
+
+console.log(`second star: ${secondStar(lines)}`)
